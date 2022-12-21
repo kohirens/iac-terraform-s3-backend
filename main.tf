9823b6dd56f86dcb374c787189ee854f6c84a917
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2022-12-21 12:25:19 -0500

    fix: Setting Bucket Name In Main Module

--- main.tf
+++ main.tf
@@ -1,35 +1,35 @@
 locals {
-  sharedName = "${var.environment}-${var.aws_account}-tf-backend"
+  shared_name = "${var.environment}-${var.aws_account}-tf-backend"
 }
 
 resource "aws_s3_bucket" "backend_logs" {
-  bucket        = "${local.sharedName}-logs"
+  bucket        = "${local.shared_name}-logs"
   force_destroy = var.force_destroy
 }
 
 resource "aws_s3_bucket_acl" "backend_logs_acl" {
-  bucket = aws_s3_bucket.backend_logs
+  bucket = aws_s3_bucket.backend_logs.bucket
   acl    = "log-delivery-write"
 }
 
 resource "aws_s3_bucket_versioning" "backend_logs_versioning" {
-  bucket = aws_s3_bucket.backend_logs
+  bucket = aws_s3_bucket.backend_logs.bucket
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "backend_logs_lifecycle" {
-  bucket = aws_s3_bucket.backend_logs
+  bucket = aws_s3_bucket.backend_logs.bucket
   rule {
-    id     = "${local.sharedName}-logs-rule"
+    id     = "${local.shared_name}-logs-rule"
     status = "Enabled"
 
     filter {
       and {
         prefix = var.access_log_prefix
         tags = {
-          rule      = "${local.sharedName}-logs-rule"
+          rule      = "${local.shared_name}-logs-rule"
           autoclean = "true"
         }
       }
@@ -61,23 +61,23 @@ resource "aws_s3_bucket_public_access_block" "backend_logs_exclude_public" {
 }
 
 resource "aws_s3_bucket" "backend" {
-  bucket        = local.sharedName
+  bucket        = local.shared_name
   force_destroy = false
 }
 
 resource "aws_s3_bucket_acl" "backend_acl" {
-  bucket = aws_s3_bucket.backend
+  bucket = aws_s3_bucket.backend.bucket
   acl    = "private"
 }
 
 resource "aws_s3_bucket_logging" "backend_logging" {
-  bucket        = aws_s3_bucket.backend
+  bucket        = aws_s3_bucket.backend.bucket
   target_bucket = aws_s3_bucket.backend_logs.bucket
   target_prefix = var.access_log_prefix
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "backend_sse" {
-  bucket = aws_s3_bucket.backend
+  bucket = aws_s3_bucket.backend.bucket
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "aws:kms"
@@ -87,7 +87,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "backend_sse" {
 }
 
 resource "aws_s3_bucket_versioning" "backend_versioning" {
-  bucket = aws_s3_bucket.backend
+  bucket = aws_s3_bucket.backend.bucket
   versioning_configuration {
     status = "Enabled"
   }
@@ -102,7 +102,7 @@ resource "aws_s3_bucket_public_access_block" "backend_bucket_exclude_public" {
 }
 
 resource "aws_dynamodb_table" "backend_table" {
-  name         = "${local.sharedName}-lock-table"
+  name         = "${local.shared_name}-lock-table"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
