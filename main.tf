88132abbec9cb2ef86192d1e03c28d6b46b0562b
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2022-12-23 12:34:40 -0500

    chg: Renamed Resources
    Updated the names to be clear what its for and which are grouped
    together. There are moved blocks for all the resources. However,
    this is still a breaking change in some cases, since this is
    the second time the resources have been renamed. Also the
    upgrade to latest providers has broken some parts of the S3
    bucket out into their own resources for them to be tracked.
    
    BREAKING CHANGE

--- main.tf
+++ main.tf
@@ -3,49 +3,44 @@ locals {
 }
 
 moved {
-  from = aws_dynamodb_table.iac_table
-  to   = aws_dynamodb_table.backend_table
+  from = aws_s3_bucket.backend_logs
+  to   = aws_s3_bucket.iac_logs
 }
 
-moved {
-  from = aws_s3_bucket.iac_bucket
-  to   = aws_s3_bucket.backend
-}
-
-moved {
-  from = aws_s3_bucket.iac_logs
-  to   = aws_s3_bucket.backend_logs
-}
-
-moved {
-  from = aws_s3_bucket_public_access_block.iac_bucket_exclude_public
-  to   = aws_s3_bucket_public_access_block.backend_bucket_exclude_public
-}
-
-moved {
-  from = aws_s3_bucket_public_access_block.iac_logs_exclude_public
-  to   = aws_s3_bucket_public_access_block.backend_logs_exclude_public
-}
-
-resource "aws_s3_bucket" "backend_logs" {
+resource "aws_s3_bucket" "iac_logs" {
   bucket        = "${local.shared_name}-logs"
   force_destroy = var.force_destroy
 }
 
-resource "aws_s3_bucket_acl" "backend_logs_acl" {
-  bucket = aws_s3_bucket.backend_logs.bucket
+moved {
+  from = aws_s3_bucket_acl.backend_logs_acl
+  to   = aws_s3_bucket_acl.iac_logs
+}
+
+resource "aws_s3_bucket_acl" "iac_logs" {
+  bucket = aws_s3_bucket.iac_logs.bucket
   acl    = "log-delivery-write"
 }
 
-resource "aws_s3_bucket_versioning" "backend_logs_versioning" {
-  bucket = aws_s3_bucket.backend_logs.bucket
+moved {
+  from = aws_s3_bucket_versioning.backend_logs_versioning
+  to   = aws_s3_bucket_versioning.iac_logs
+}
+
+resource "aws_s3_bucket_versioning" "iac_logs" {
+  bucket = aws_s3_bucket.iac_logs.bucket
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "backend_logs_lifecycle" {
-  bucket = aws_s3_bucket.backend_logs.bucket
+moved {
+  from = aws_s3_bucket_lifecycle_configuration.backend_logs_lifecycle
+  to   = aws_s3_bucket_lifecycle_configuration.iac_logs
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "iac_logs" {
+  bucket = aws_s3_bucket.iac_logs.bucket
   rule {
     id     = "${local.shared_name}-logs-rule"
     status = "Enabled"
@@ -59,7 +54,6 @@ resource "aws_s3_bucket_lifecycle_configuration" "backend_logs_lifecycle" {
         }
       }
     }
-
 
     transition {
       days          = 45
@@ -77,32 +71,57 @@ resource "aws_s3_bucket_lifecycle_configuration" "backend_logs_lifecycle" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "backend_logs_exclude_public" {
-  bucket                  = aws_s3_bucket.backend_logs.bucket
+moved {
+  from = aws_s3_bucket_public_access_block.backend_logs_exclude_public
+  to   = aws_s3_bucket_public_access_block.iac_logs
+}
+
+resource "aws_s3_bucket_public_access_block" "iac_logs" {
+  bucket                  = aws_s3_bucket.iac_logs.bucket
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket" "backend" {
+moved {
+  from = aws_s3_bucket.backend
+  to   = aws_s3_bucket.tf_state
+}
+
+resource "aws_s3_bucket" "tf_state" {
   bucket        = local.shared_name
   force_destroy = false
 }
 
-resource "aws_s3_bucket_acl" "backend_acl" {
-  bucket = aws_s3_bucket.backend.bucket
+moved {
+  from = aws_s3_bucket_acl.backend_acl
+  to   = aws_s3_bucket_acl.tf_state
+}
+
+resource "aws_s3_bucket_acl" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.bucket
   acl    = "private"
 }
 
-resource "aws_s3_bucket_logging" "backend_logging" {
-  bucket        = aws_s3_bucket.backend.bucket
-  target_bucket = aws_s3_bucket.backend_logs.bucket
+moved {
+  from = aws_s3_bucket_logging.backend_logging
+  to   = aws_s3_bucket_logging.tf_state
+}
+
+resource "aws_s3_bucket_logging" "tf_state" {
+  bucket        = aws_s3_bucket.tf_state.bucket
+  target_bucket = aws_s3_bucket.iac_logs.bucket
   target_prefix = var.access_log_prefix
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "backend_sse" {
-  bucket = aws_s3_bucket.backend.bucket
+moved {
+  from = aws_s3_bucket_server_side_encryption_configuration.backend_sse
+  to   = aws_s3_bucket_server_side_encryption_configuration.tf_state
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.bucket
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "aws:kms"
@@ -111,22 +130,37 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "backend_sse" {
   }
 }
 
-resource "aws_s3_bucket_versioning" "backend_versioning" {
-  bucket = aws_s3_bucket.backend.bucket
+moved {
+  from = aws_s3_bucket_versioning.backend_versioning
+  to   = aws_s3_bucket_versioning.tf_state
+}
+
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.bucket
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "backend_bucket_exclude_public" {
-  bucket                  = aws_s3_bucket.backend.bucket
+moved {
+  from = aws_s3_bucket_public_access_block.backend_bucket_exclude_public
+  to   = aws_s3_bucket_public_access_block.tf_state
+}
+
+resource "aws_s3_bucket_public_access_block" "tf_state" {
+  bucket                  = aws_s3_bucket.tf_state.bucket
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-resource "aws_dynamodb_table" "backend_table" {
+moved {
+  from = aws_dynamodb_table.backend_table
+  to   = aws_dynamodb_table.tf_state_lock
+}
+
+resource "aws_dynamodb_table" "tf_state_lock" {
   name         = "${local.shared_name}-lock-table"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
--- output.tf
+++ output.tf
@@ -1,85 +1,85 @@
 output "logs_bucket_arn" {
   description = "The ARN of the bucket that stores terraform logs."
-  value       = aws_s3_bucket.backend_logs.arn
+  value       = aws_s3_bucket.iac_logs.arn
 }
 
 output "logs_bucket_id" {
   description = "The ID of the bucket that stores terraform logs."
-  value       = aws_s3_bucket.backend_logs.id
+  value       = aws_s3_bucket.iac_logs.id
 }
 
 output "logs_bucket_domain_name" {
   description = "The bucket domain of the bucket that stores terraform logs."
-  value       = aws_s3_bucket.backend_logs.bucket_domain_name
+  value       = aws_s3_bucket.iac_logs.bucket_domain_name
 }
 
 output "logs_bucket_hosted_zone_id" {
   description = "The Route 53 hosted zone ID of the bucket that stores terraform logs."
-  value       = aws_s3_bucket.backend_logs.hosted_zone_id
+  value       = aws_s3_bucket.iac_logs.hosted_zone_id
 }
 
 output "logs_bucket_name" {
   description = "The name of the bucket that stores terraform logs."
-  value       = aws_s3_bucket.backend_logs.bucket
+  value       = aws_s3_bucket.iac_logs.bucket
 }
 
 output "logs_bucket_region" {
   description = "The AWS region of the bucket that stores terraform logs."
-  value       = aws_s3_bucket.backend_logs.region
+  value       = aws_s3_bucket.iac_logs.region
 }
 
 output "logs_bucket_regional_domain_name" {
   description = "The bucket region-specific domain name of the bucket that stores terraform logs."
-  value       = aws_s3_bucket.backend_logs.bucket_regional_domain_name
+  value       = aws_s3_bucket.iac_logs.bucket_regional_domain_name
 }
 
 output "state_bucket_arn" {
   description = "The ARN of the bucket that stores state files."
-  value       = aws_s3_bucket.backend.arn
+  value       = aws_s3_bucket.tf_state.arn
 }
 
 output "state_bucket_id" {
   description = "The ID of the bucket that stores state files."
-  value       = aws_s3_bucket.backend.id
+  value       = aws_s3_bucket.tf_state.id
 }
 
 output "state_bucket_domain_name" {
   description = "The bucket domain name of the bucket that stores state files."
-  value       = aws_s3_bucket.backend.bucket_domain_name
+  value       = aws_s3_bucket.tf_state.bucket_domain_name
 }
 
 output "state_bucket_hosted_zone_id" {
   description = "The Route 53 Hosted Zone ID  of the bucket that stores state files."
-  value       = aws_s3_bucket.backend.hosted_zone_id
+  value       = aws_s3_bucket.tf_state.hosted_zone_id
 }
 
 output "state_bucket_name" {
   description = "The name of the bucket that stores state files."
-  value       = aws_s3_bucket.backend.bucket
+  value       = aws_s3_bucket.tf_state.bucket
 }
 
 output "state_bucket_regional_domain_name" {
   description = "The bucket region-specific domain name of the bucket that stores state files."
-  value       = aws_s3_bucket.backend.bucket_regional_domain_name
+  value       = aws_s3_bucket.tf_state.bucket_regional_domain_name
 }
 
 
 output "state_bucket_region" {
   description = "The AWS region of the bucket that stores state files."
-  value       = aws_s3_bucket.backend.region
+  value       = aws_s3_bucket.tf_state.region
 }
 
 output "table_arn" {
   description = "DynamoDB table ARN for locking the state file."
-  value       = aws_dynamodb_table.backend_table.arn
+  value       = aws_dynamodb_table.tf_state_lock.arn
 }
 
 output "table_id" {
   description = "DynamoDB table ID for locking the state file."
-  value       = aws_dynamodb_table.backend_table.id
+  value       = aws_dynamodb_table.tf_state_lock.id
 }
 
 output "table_name" {
   description = "DynamoDB table name for locking the state file."
-  value       = aws_dynamodb_table.backend_table.name
+  value       = aws_dynamodb_table.tf_state_lock.name
 }
