a21b42e624ed5f1961f2fe8080b6b6bea703c2d4
Khalifah K. Shabazz <shabazzk@gmail.com>
2023-11-28 22:53:31 -0500

    BREAKING CHANGE
    Removed moved resources. No longer backwards compatible.

--- main.tf
+++ main.tf
@@ -2,29 +2,15 @@ locals {
   shared_name = "${var.environment}-${var.aws_account}-tf-backend"
 }
 
-moved {
-  from = aws_s3_bucket.backend_logs
-  to   = aws_s3_bucket.iac_logs
-}
-
 resource "aws_s3_bucket" "iac_logs" {
   bucket        = "${local.shared_name}-logs"
   force_destroy = var.force_destroy
 }
 
-moved {
-  from = aws_s3_bucket_acl.backend_logs_acl
-  to   = aws_s3_bucket_acl.iac_logs
-}
 
 resource "aws_s3_bucket_acl" "iac_logs" {
   bucket = aws_s3_bucket.iac_logs.bucket
   acl    = "log-delivery-write"
-}
-
-moved {
-  from = aws_s3_bucket_versioning.backend_logs_versioning
-  to   = aws_s3_bucket_versioning.iac_logs
 }
 
 resource "aws_s3_bucket_versioning" "iac_logs" {
@@ -32,11 +18,6 @@ resource "aws_s3_bucket_versioning" "iac_logs" {
   versioning_configuration {
     status = "Enabled"
   }
-}
-
-moved {
-  from = aws_s3_bucket_lifecycle_configuration.backend_logs_lifecycle
-  to   = aws_s3_bucket_lifecycle_configuration.iac_logs
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "iac_logs" {
@@ -71,10 +52,6 @@ resource "aws_s3_bucket_lifecycle_configuration" "iac_logs" {
   }
 }
 
-moved {
-  from = aws_s3_bucket_public_access_block.backend_logs_exclude_public
-  to   = aws_s3_bucket_public_access_block.iac_logs
-}
 
 resource "aws_s3_bucket_public_access_block" "iac_logs" {
   bucket                  = aws_s3_bucket.iac_logs.bucket
@@ -84,19 +61,9 @@ resource "aws_s3_bucket_public_access_block" "iac_logs" {
   restrict_public_buckets = true
 }
 
-moved {
-  from = aws_s3_bucket.backend
-  to   = aws_s3_bucket.tf_state
-}
-
 resource "aws_s3_bucket" "tf_state" {
   bucket        = local.shared_name
   force_destroy = false
-}
-
-moved {
-  from = aws_s3_bucket_acl.backend_acl
-  to   = aws_s3_bucket_acl.tf_state
 }
 
 resource "aws_s3_bucket_acl" "tf_state" {
@@ -104,20 +71,10 @@ resource "aws_s3_bucket_acl" "tf_state" {
   acl    = "private"
 }
 
-moved {
-  from = aws_s3_bucket_logging.backend_logging
-  to   = aws_s3_bucket_logging.tf_state
-}
-
 resource "aws_s3_bucket_logging" "tf_state" {
   bucket        = aws_s3_bucket.tf_state.bucket
   target_bucket = aws_s3_bucket.iac_logs.bucket
   target_prefix = var.access_log_prefix
-}
-
-moved {
-  from = aws_s3_bucket_server_side_encryption_configuration.backend_sse
-  to   = aws_s3_bucket_server_side_encryption_configuration.tf_state
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
@@ -130,21 +87,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
   }
 }
 
-moved {
-  from = aws_s3_bucket_versioning.backend_versioning
-  to   = aws_s3_bucket_versioning.tf_state
-}
-
 resource "aws_s3_bucket_versioning" "tf_state" {
   bucket = aws_s3_bucket.tf_state.bucket
   versioning_configuration {
     status = "Enabled"
   }
-}
-
-moved {
-  from = aws_s3_bucket_public_access_block.backend_bucket_exclude_public
-  to   = aws_s3_bucket_public_access_block.tf_state
 }
 
 resource "aws_s3_bucket_public_access_block" "tf_state" {
@@ -153,11 +100,6 @@ resource "aws_s3_bucket_public_access_block" "tf_state" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
-
-moved {
-  from = aws_dynamodb_table.backend_table
-  to   = aws_dynamodb_table.tf_state_lock
 }
 
 resource "aws_dynamodb_table" "tf_state_lock" {
